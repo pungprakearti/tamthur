7c038ade9ee72fa647173d4104859fe10469b573
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2019-02-07 13:49:14 -0800

    Placed the roses and styled them.

--- src/Top.js
+++ src/Top.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import roses from './white_roses.png';
 import './Top.scss';
 
 export default class Top extends Component {
   render() {
     return (
       <div className="Top-cont">
-        <div className="Top-header">
-          Tammy<div className="Top-and">and</div>Arthur
+        <div className="Top-header-cont">
+          <div className="Top-header">
+            <img
+              className="Top-roses"
+              src={roses}
+              alt="white vintage looking roses"
+            />
+            <div className="Top-header-text">
+              Tammy<div className="Top-and">and</div>Arthur
+            </div>
+          </div>
+          <div className="Top-date">{this.props.date}</div>
+          <div className="Top-venue">{this.props.venue.name}</div>
         </div>
-        <div className="Top-date">{this.props.date}</div>
-        <div className="Top-venue">{this.props.venue.name}</div>
         <div className="Top-navbar">
           <div className="Top-navbar-text">
             <NavLink to="/">Home</NavLink>
